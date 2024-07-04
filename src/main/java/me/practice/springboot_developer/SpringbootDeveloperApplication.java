9e4e2461558038818c3c5413d38b6f18998dfcf7
chorock33 <chorock33@gmail.com>
2024-07-04 16:52:15 +0900

    initial commit

---
+++ src/main/java/me/practice/springboot_developer/SpringbootDeveloperApplication.java
@@ -0,0 +1,14 @@
+package me.practice.springboot_developer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDeveloperApplication.class, args);
+		// commit test
+	}
+
+}
---
+++ src/test/java/me/practice/springboot_developer/SpringbootDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package me.practice.springboot_developer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
